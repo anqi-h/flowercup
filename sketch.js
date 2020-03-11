c14bf2f494b059d49450e83d5084bafa3632313c
anqi-h <60205281+anqi-h@users.noreply.github.com>
2020-03-11 23:12:49 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+// cup Object
+let cup = {
+  state: "Placed",
+}
+
+
+function setup() {
+  //createCanvas(800, 600);
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+
+  const connectButton = createButton("Connect");
+  connectButton.mousePressed(connectButtonPressed);
+
+  const disconnectButton = createButton("Disconnect");
+  disconnectButton.mousePressed(disconnectButtonPressed);
+}
+
+function handleData(status) {
+
+  if (cup.state !== status) { //check if status is different from previous
+
+    if (status === "Placed") {
+      statePlace = createImg("videos/flower1.gif")
+      statePlace.position(50, 50); 
+    } else if (status === "Lifted") {
+      stateLift = createImg("videos/flower2.gif")
+      stateLift.position(50, 50);
+    } else if (status === "Pouring") {
+      statePour = createImg("videos/flower3.gif")
+      statePour.position(50, 50);
+    }
+
+    cup.state = status; //update the status
+    print("current state:" + status);
+  }
+}
+
+
+function keyPressed() {
+  if (keyCode === 32) { //press SPACE BAR to turn on fullscreen.
+    toggleFullScreen();
+  }
+}
+
+function toggleFullScreen() {
+  let fs = fullscreen();
+  fullscreen(!fs);
+}
